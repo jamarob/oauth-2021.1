65e76644a5ddcc7ee6c4085230606e7da191b8b0
Jan <janmartinrobert@gmail.com>
2021-03-19 10:37:49 +0100

    Add github login url

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,11 @@
+const clientId = process.env.REACT_APP_GITHUB_CLIENT_ID
+const githubLoginUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}`
+
 export default function Login() {
   return (
     <>
       <h1>Login</h1>
-      <a href="https://www.github.com">Login with Github</a>
+      <a href={githubLoginUrl}>Login with Github</a>
     </>
   )
 }
